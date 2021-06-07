1c509cbb5b703cd3b4e5f90f1d6ba0225f0f58c1
bethcreate <kamaubetth@gmail.com>
2021-06-07 21:19:21 +0300

    first commit

--- bank.py
+++ bank.py
@@ -7,6 +7,7 @@ class BankAccount:
         self.phone_number=phone_number
         self.balance=0
         self.statement= []
+        self.loan=0
     def show_balance(self):
         return f"hello{self.name} your balance is {self.balance}"
     def deposit(self, amount):
@@ -48,6 +49,66 @@ class BankAccount:
             date = time.strftime("%d/%m/%y")
             print(f"{date} {Naration} {amount}")
         return self.statement
+
+    def borrow(self,amount):
+        if amount<0:
+            return f"You cannot borrow more than limit"
+
+        elif self.loan>=0:
+            return f"You can borrow"
+
+        elif amount<0.1*self.balance:
+            return f"You have reached to your loan limit"
+        
+        else:
+
+            loan=amount*1.05
+            self.loan=loan
+            self.balance +=amount
+            now=datetime.now()
+            Borrow_transaction={"amount":amount, "time": now, "Naration": "You borrowed"}
+            self.statement.append(Borrow_transaction)
+
+            return f"You have to repay the existing loan" 
+
+
+    def repay(self,amount):
+        if amount<0:
+            return f"Your balance is too low! "
+        
+        elif amount <= self.loan:
+            return f"decrease loan with {amount}"
+
+        elif amount< self.loan:
+            self.loan-=amount
+            return "You have reduced your loan"
+
+        else:
+            diff=amount-self.loan
+            self.loan=0
+            self.deposit(diff)
+            now=datetime.now()
+            repay_transaction={"amount":amount, "time": now, "Naration": "You have repayed"}
+            self.statement.append(repay_transaction)
+            return f"Your loan is fully repaid"
+
+         
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
 
     
 
